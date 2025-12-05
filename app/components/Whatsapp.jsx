95cc279ba5a9f900f6a451771b4f3d2d9c045d72
Akbhardwaj5959 <riyajay31@gmail.com>
2025-12-05 21:45:06 +0530

    whatsapp

--- app/components/Whatsapp.jsx
+++ app/components/Whatsapp.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function WhatsappFloat() {
   return (
     <a
-      href="https://wa.me/919625145845?text=Hello%20I%20want%20to%20know%20more%20about%20your%20classes"
+      href="https://wa.me/918287651345?text=Hello%20I%20want%20to%20know%20more%20about%20your%20classes"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed z-50 bottom-4 right-4 sm:bottom-6 sm:right-6 block"
